3d3ab5d83752448654bc17d333cec95f451efc3b
alanlin8581 <43751729+alanlin8581@users.noreply.github.com>
2019-01-31 16:19:38 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,14 @@
+$("#button1").click(function() {
+    $(".image2").fadeToggle();
+    
+});
+
+$("#button2").click(function() {
+    $(".image4").hide();
+    
+});
+
+$("#button3").click(function() {
+    $(".image5").toggle();
+
+});
